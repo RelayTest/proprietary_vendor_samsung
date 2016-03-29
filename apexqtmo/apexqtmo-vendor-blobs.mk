99320a839e11e3162714100ad0d93ff6d45c819f
Tom Marshall <tdm@cyngn.com>
2016-03-29 14:58:10 -0700

    apexqtmo: Separate blobs from msm8960-common
    Change-Id: I34cd86e327981a72b87daa6a76f0635d1be7f37b

--- apexqtmo/apexqtmo-vendor-blobs.mk
+++ apexqtmo/apexqtmo-vendor-blobs.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2014-2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,12 +20,33 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/apexqtmo/proprietary/etc/thermald.conf:system/etc/thermald.conf \
     vendor/samsung/apexqtmo/proprietary/lib/libcordon.so:system/lib/libcordon.so \
     vendor/samsung/apexqtmo/proprietary/lib/libqc-opt.so:system/lib/libqc-opt.so \
+    vendor/samsung/apexqtmo/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
+    vendor/samsung/apexqtmo/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+    vendor/samsung/apexqtmo/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+    vendor/samsung/apexqtmo/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+    vendor/samsung/apexqtmo/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
+    vendor/samsung/apexqtmo/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libCB.so:system/lib/libCB.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libgsl.so:system/lib/libgsl.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libOpenCL.so:system/lib/libOpenCL.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
+    vendor/samsung/apexqtmo/proprietary/etc/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
+    vendor/samsung/apexqtmo/proprietary/etc/firmware/a225_pfp.fw:system/etc/firmware/a225_pfp.fw \
+    vendor/samsung/apexqtmo/proprietary/etc/firmware/a225_pm4.fw:system/etc/firmware/a225_pm4.fw \
+    vendor/samsung/apexqtmo/proprietary/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
+    vendor/samsung/apexqtmo/proprietary/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
+    vendor/samsung/apexqtmo/proprietary/etc/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
+    vendor/samsung/apexqtmo/proprietary/etc/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \
     vendor/samsung/apexqtmo/proprietary/lib/libacdbloader.so:system/lib/libacdbloader.so \
     vendor/samsung/apexqtmo/proprietary/lib/libaudcal.so:system/lib/libaudcal.so \
     vendor/samsung/apexqtmo/proprietary/lib/libaudioalsa.so:system/lib/libaudioalsa.so \
     vendor/samsung/apexqtmo/proprietary/lib/libcsd-client.so:system/lib/libcsd-client.so \
     vendor/samsung/apexqtmo/proprietary/bin/btnvtool:system/bin/btnvtool \
     vendor/samsung/apexqtmo/proprietary/bin/hci_qcomm_init:system/bin/hci_qcomm_init \
+    vendor/samsung/apexqtmo/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
     vendor/samsung/apexqtmo/proprietary/lib/hw/camera.vendor.msm8960.so:system/lib/hw/camera.vendor.msm8960.so \
     vendor/samsung/apexqtmo/proprietary/lib/libchromatix_s5k3l1yx_default_video.so:system/lib/libchromatix_s5k3l1yx_default_video.so \
     vendor/samsung/apexqtmo/proprietary/lib/libchromatix_s5k3l1yx_preview.so:system/lib/libchromatix_s5k3l1yx_preview.so \
@@ -35,11 +56,17 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/apexqtmo/proprietary/lib/libimage-jpeg-dec-omx-comp.so:system/lib/libimage-jpeg-dec-omx-comp.so \
     vendor/samsung/apexqtmo/proprietary/lib/libimage-jpeg-enc-omx-comp.so:system/lib/libimage-jpeg-enc-omx-comp.so \
     vendor/samsung/apexqtmo/proprietary/lib/libimage-omx-common.so:system/lib/libimage-omx-common.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libmmcamera_faceproc.so:system/lib/libmmcamera_faceproc.so \
     vendor/samsung/apexqtmo/proprietary/lib/libmmcamera_frameproc.so:system/lib/libmmcamera_frameproc.so \
     vendor/samsung/apexqtmo/proprietary/lib/libmmcamera_statsproc30.so:system/lib/libmmcamera_statsproc30.so \
     vendor/samsung/apexqtmo/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libmmmpod.so:system/lib/libmmmpod.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libmmstereo.so:system/lib/libmmstereo.so \
     vendor/samsung/apexqtmo/proprietary/lib/libmmstillomx.so:system/lib/libmmstillomx.so \
     vendor/samsung/apexqtmo/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
+    vendor/samsung/apexqtmo/proprietary/cameradata/datapattern_420sp.yuv:system/cameradata/datapattern_420sp.yuv \
+    vendor/samsung/apexqtmo/proprietary/cameradata/datapattern_front_420sp.yuv:system/cameradata/datapattern_front_420sp.yuv \
     vendor/samsung/apexqtmo/proprietary/lib/hw/nfc.msm8960.so:system/lib/hw/nfc.msm8960.so \
     vendor/samsung/apexqtmo/proprietary/vendor/firmware/libpn544_fw.so:system/vendor/firmware/libpn544_fw.so \
     vendor/samsung/apexqtmo/proprietary/lib/libqdi.so:system/lib/libqdi.so \
@@ -80,6 +107,8 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/apexqtmo/proprietary/lib/libinvensense_hal.so:system/lib/libinvensense_hal.so \
     vendor/samsung/apexqtmo/proprietary/lib/libmllite.so:system/lib/libmllite.so \
     vendor/samsung/apexqtmo/proprietary/lib/libmlplatform.so:system/lib/libmlplatform.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libmplmpu.so:system/lib/libmplmpu.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libyamaha.so:system/lib/libyamaha.so \
     vendor/samsung/apexqtmo/proprietary/etc/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept \
     vendor/samsung/apexqtmo/proprietary/etc/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny \
     vendor/samsung/apexqtmo/proprietary/etc/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
@@ -89,4 +118,11 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/apexqtmo/proprietary/etc/firmware/wcnss.b02:system/etc/firmware/wcnss.b02 \
     vendor/samsung/apexqtmo/proprietary/etc/firmware/wcnss.b04:system/etc/firmware/wcnss.b04 \
     vendor/samsung/apexqtmo/proprietary/etc/firmware/wcnss.b05:system/etc/firmware/wcnss.b05 \
-    vendor/samsung/apexqtmo/proprietary/etc/firmware/wcnss.mdt:system/etc/firmware/wcnss.mdt
+    vendor/samsung/apexqtmo/proprietary/etc/firmware/wcnss.mdt:system/etc/firmware/wcnss.mdt \
+    vendor/samsung/apexqtmo/proprietary/lib/libdivxdrm.so:system/lib/libdivxdrm.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libdivxdrmdecrypt.so:system/lib/libdivxdrmdecrypt.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libI420colorconvert.so:system/lib/libI420colorconvert.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libmm-color-convertor.so:system/lib/libmm-color-convertor.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
+    vendor/samsung/apexqtmo/proprietary/lib/libwvm.so:system/lib/libwvm.so
